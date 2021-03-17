df5b078fa7324b52c92b1860a1b227696156ba37
devendradd <this.devendra@gmail.com>
2021-03-17 19:14:35 +0530

    items container

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import HooksCakeContainer from './compoenents/HooksCakeContainer';
 import IceCreamContainer from './compoenents/IceCreamContainer';
 import ColdDrinkContainer from './compoenents/ColdDrinkContainer'; 
 import JalebiCOntainer from './compoenents/JalebiCOntainer';
+import NewCakeContainer from './compoenents/NewCakeContainer';
+import ItemContainer from './compoenents/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App"> 
+        <ItemContainer cake/>
+        <ItemContainer />
         <CakeContainer/>
         <HooksCakeContainer/>
         <IceCreamContainer />
         <ColdDrinkContainer />
         <JalebiCOntainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
